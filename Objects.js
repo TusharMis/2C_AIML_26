ab7f536f77430220b8d8165cc471a6525d2e996b
TusharMis <mishratushar0610@gmail.com>
2026-08-20 15:32:53 +0530

    Objects

---
+++ Objects.js
@@ -0,0 +1,10 @@
+let student = new  Object();
+
+student.name = "Tushar";
+student.id = 45;
+student.city = "Chitrakoot";
+student.Learning = function() {
+    console.log("Learning Course");
+}
+console.log(student.name);
+student.Learning();
