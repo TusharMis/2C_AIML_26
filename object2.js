926e92d63632d68b897aa70ecb9d362ab6a764fb
TusharMis <mishratushar0610@gmail.com>
2026-08-20 16:05:06 +0530

    Constructor

---
+++ object2.js
@@ -0,0 +1,9 @@
+function student(id, name, city){
+         this.id = id;
+         this.name = name;
+         this.city = city;
+}
+let st1  = new student(101, "Tushar", "Chitrakoot")
+let st2 = new student(212, "Panchu", "Chitrakoot")
+console.log(st1)
+console.log(st2)
