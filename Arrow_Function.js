06cb271cc194b2cec4490d4a7353a5a9d515817b
TusharMis <mishratushar0610@gmail.com>
2026-09-03 16:19:24 +0530

    rest Opretor

--- Arrow_Function.js
+++ Arrow_Function.js
@@ -13,3 +13,27 @@ console.log(sum())
 }
 
 console.log(fullName("Tushar", "Mishra"))
+ 
+// rest opretor
+
+function restOp(...value){
+    console.log(value)
+     let total = 0
+    for(let n of value){
+        total = total + n
+    }
+    console.log(total)
+}
+
+restOp(12,13,34,12)
+
+function sum(name, ...value){
+    let total = 0;
+    for(let n of value){
+        total = total+n;
+    }
+    console.log(name, total);
+    
+}
+
+console.log(sum("Tusar", 10,12,21));
