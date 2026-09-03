c3a7926ada07470ec8a6ed7bb1a5dd384778e033
TusharMis <mishratushar0610@gmail.com>
2026-09-03 15:56:01 +0530

    Arrow function

---
+++ Arrow_Function.js
@@ -0,0 +1,15 @@
+sum = () => {
+    let a = 10;
+    let b = 20;
+    c = a+b;
+    return c
+}
+
+console.log(sum())
+
+
+ const fullName = (firstName, lastName) => {
+ return `${firstName} ${lastName}`
+}
+
+console.log(fullName("Tushar", "Mishra"))
---
+++ Netflix/ATM_bank.js
@@ -0,0 +1,1 @@
+let username = "Tushar_mis"
